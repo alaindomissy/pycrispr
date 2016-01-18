295c95def88f8621ff70967485c3848f794c8ea5
Alain <alaindomissy@gmail.com>
2016-01-18 09:04:24 -0800

    commented out datadir in payload.py

--- pybasespace/payload.py
+++ pybasespace/payload.py
@@ -9,7 +9,7 @@ SCAFFOLDS =  os.environ.get('SCAFFOLDS','/root/scaffolds/mm8/')
 
 
 
-datadirpath = "/data/"
+# datadirpath = "/data/"
 #
 # main.blast(datadirpath, 'chr6:136640001-136641000_1000.prsp', '/BLASTDB/', 'mm8')
 #
@@ -18,11 +18,9 @@ datadirpath = "/data/"
 # DIRECT = '/data/scaffolds/mm8/'
 # cut.create_cutbedlines_from_seq_file(DIRECT, 'chr6-1-400', 'fasta')
 #
-#
 # from buffet import digest
 # DIRECT = '/data/scaffolds/hg38/'
 # digest.digest_file(DIRECT, 'hg38', 'fasta')
-#
 #
 # digest.digest_coord('./', 'chr21:42774475-42905100','mm8', 'fasta')
 #
