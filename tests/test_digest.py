d15d69f83a4d5b9dc88d5e48b9dec7092e8d1bd1
alaindomissy <alaindomissy@gmail.com>
2016-01-19 23:16:11 -0800

    gitignore .idea folder, fixed a test in test_digest

--- tests/test_digest.py
+++ tests/test_digest.py
@@ -7,12 +7,12 @@ import buffet.digest as digest
 
 def test_coord_to_bedtuple_filename():
     assert(digest.coord_to_bedtuple_filename('chr6:136640001-136680000') ==
-        ([('chr6', '136640000', '136680000')], 'chr6:136640001-136680000_40000')
+        ([('chr6', '136640000', '136680000')], 'chr6+136640001-136680000_40000')
     )
 
 
 def digesttest_stretch_to_bedtuple_filename():
     assert(stretch_to_bedtuple_filename('chr6:136640001_40000') ==
-        ([('chr6', '136640000', '136680000')], 'chr6:136640001-136680000_40000')
+        ([('chr6', '136640000', '136680000')], 'chr6+136640001-136680000_40000')
     )
 
