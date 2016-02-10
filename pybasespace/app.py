38cda1083bce348ee2c15c12a030ba1010bbad3e
alaindomissy <alaindomissy@gmail.com>
2016-02-10 10:27:29 -0800

    debugging APPSESS

--- pybasespace/app.py
+++ pybasespace/app.py
@@ -175,6 +175,14 @@ def process_appsession(param_values):
 
 if __name__ == '__main__':
 
+    print("APPSESS", APPSESS )
+    print("type(APPSESS)", type(APPSESS))
+    assert(APPSESS == '/data/input/AppSession.json')
+    import os
+    print(">>>os.listdir('/data/input/AppSession.json')")
+    print(os.listdir('/data/input/AppSession.json'))
+
+    appsessionhref1, appsessionparams1 = read_appsession('/data/input/AppSession.json')
     appsessionhref, appsessionparams = read_appsession(APPSESS)
     param_values = parse_appsessionparams(appsessionparams)
     process_appsession(param_values)
