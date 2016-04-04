1dcfff6a333c4349719d3a1c25c09cb63a31cf4a
alaindomissy <alaindomissy@gmail.com>
2016-04-04 05:03:54 -0700

    ARGUMENTS_WITH_CONTENT _WITH _ITEMS  moved to payload.py

--- crispr/payload.py
+++ crispr/payload.py
@@ -10,9 +10,22 @@ from __future__ import absolute_import, division, print_function   # , unicode_l
 from shutil import copytree
 from datetime import datetime
 from basespaceapp.app import main as appmain
-from .config import SCRATCH, restore_genome, ARGUMENTS_WITH_CONTENT, ARGUMENTS_WITH_ITEMS  # ,\
-    # DIGEST_LOG_ON, BLAST_LOG_ON, SCORE_LOG_ON, STRETCH_LOG_ON, PRIME_LOG_ON
+from .config import SCRATCH, restore_genome
 from .main import digest_blast_score_cluster_prime
+
+
+ARGUMENTS_WITH_CONTENT = [
+                            # 'input.interference_gap',
+                            # 'input.blast_chunk_size',
+                            # 'input.blast_max_hsps',
+                            'input.refgenome_id',
+                            'input.genomic_coord',
+                            'input.low_threshold',
+                            'input.high_threshold',
+                            'input.primer_screening'
+                         ]
+
+ARGUMENTS_WITH_ITEMS = ['input.restr_enzymes', 'input.checkbox_logging']
 
 
 ###################
@@ -21,30 +34,44 @@ from .main import digest_blast_score_cluster_prime
 
 def payload(params_value, output_dir):
 
+    params_value = params_value or dict(
+        {'input.samples': [],
+          u'input.primer_screening': u'dumb',
+          'input.projects': [{'href': u'v1pre3/projects/29085057', 'id': u'29085057', 'name': u'crispr-ecoli'}],
+          u'input.restr_enzymes': [u'BfaI', u'ScrFI', u'HpaII'],
+          u'input.checkbox_logging': [u'stretch_log_on', u'prime_log_on'],
+          'output.projects': [{'href': u'v1pre3/projects/29085057', 'id': u'29085057', 'name': u'crispr-ecoli'}],
+          u'input.refgenome_id': u'ecoli',
+          u'input.genomic_coord': u'chr:102000-106999',
+          u'input.high_threshold': u'99',
+          u'input.low_threshold': u'75'
+        })
+
+    output_dir = output_dir or '/data/'
+
     print('params_value', params_value)
     print('output_dir', output_dir)
 
     coord = str(params_value.get('input.genomic_coord'))
     genome = str(params_value['input.refgenome_id'])
 
-    restriction_enzymes =  params_value['input.restr_enzymes']
-
     chunk_size = 10                  #int(params_value['input.blast_chunk_size'])
     max_hsps = 10                    # int(params_value['input.blast_max_hsps'])
     low_threshold = params_value['input.low_threshold']     #75
     high_threshold = params_value['input.high_threshold']   #94
-
-
     # interference_gap = str(params_value['input.interference_gap'])
     # primer_screening = params_value['input.primer_screening']
 
+
+    restriction_enzymes =  params_value['input.restr_enzymes']
+
     checkbox_logging = params_value['input.checkbox_logging']
+
     DIGEST_LOG_ON = 'digest_log_on' in checkbox_logging
     BLAST_LOG_ON = 'blast_log_on' in checkbox_logging
     SCORE_LOG_ON = 'score_log_on' in checkbox_logging
     STRETCH_LOG_ON = 'stretch_log_on' in checkbox_logging
     PRIME_LOG_ON = 'prime_log_on' in checkbox_logging
-
 
 
     restore_genome(genome)
@@ -56,7 +83,7 @@ def payload(params_value, output_dir):
     # sessiondetails_dir = output_dir + '../sessiondetails/'
     sessiondetails_dir = output_dir + '../sessiondetails_' + datetimenow + '/'
     print('sessiondetails_dir', sessiondetails_dir)
-    # copytree(source, destination, ignore=_logpath)
+    # copytree(source, destination, ignore=_logpath)    # TODO should we use the thrd parameterto copytree
     # TODO why copytree is not working from here? currently it is called from basespaceapp.app.process_appsession
     # copytree(SCRATCH, sessiondetails_dir)
 
